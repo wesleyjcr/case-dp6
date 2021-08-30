dca318ed268af887099474a15d7b6a0b572caf41
wesleyjcr <wesleyjcr@gmail.com>
2021-08-30 19:45:35 -0300

    Inserção de tags Analytics

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,9 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WNZWNWQV9R');
